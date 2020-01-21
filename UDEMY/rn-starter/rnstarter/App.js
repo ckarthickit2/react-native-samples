51cc934d43284352896c1e4d49a0d24a08c76c72
CHINNATHAMBI, KARTHICK (kc3503) <karthick.chinnathambi@intl.att.com>
2020-01-21 17:30:26 +0530

    Lazy Load component Demo

--- UDEMY/rn-starter/rnstarter/App.js
+++ UDEMY/rn-starter/rnstarter/App.js
@@ -1,10 +1,10 @@
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
-import HomeScreen from './src/screens/HomeScreen';
+//import HomeScreen from './src/screens/HomeScreen';
 
 const navigator = createStackNavigator(
   {
-    Home: HomeScreen,
+    Home: require('./src/screens/HomeScreen').default, //Lazy Loading using require instead of import
   },
   {
     initialRouteName: 'Home',
